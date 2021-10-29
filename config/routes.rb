3be15ca955fe041988b3aa5da26548ca858f041a
rikuzyoukurabu <rikuzyoukurabu@gmail.com>
2021-10-29 15:02:51 +0000

    admin userログイン画面まで

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  namespace :admin do
+    root to: "homes#top"
+  end
+
+  scope module: :user do
+    root to: "homes#top"
+    get "/about" => "homes#about"
+  end
+
+  devise_for :users,skip:[:passwords],controllers: {
+    sessions:      'user/sessions',
+    registrations: 'user/registrations'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords],controllers: {
+    sessions: 'admin/sessions'
+  }
+end
