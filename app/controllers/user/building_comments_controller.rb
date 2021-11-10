1f4d217eb261ab92e96b83bcc60a044ce4710736
rikuzyoukurabu <rikuzyoukurabu@gmail.com>
2021-11-10 06:21:33 +0000

    [Add]Buildingコメント非同期

--- app/controllers/user/building_comments_controller.rb
+++ app/controllers/user/building_comments_controller.rb
@@ -1,2 +1,35 @@
-class User::BuidingCommentsController < ApplicationController
+class User::BuildingCommentsController < ApplicationController
+  def create
+    @building = Building.find(params[:building_id])
+    @building_comment = BuildingComment.new(building_comment_params)
+    @building_comment.user_id = current_user.id
+    @building_comment.building_id = @building.id
+    @comments = @building.building_comments.order(created_at: :desc)
+
+    @building_comment.save
+
+    # redirect_to toilet_path(@toilet)
+    render :index
+
+  end
+
+  def destroy
+    @building = Building.find(params[:building_id])
+    @building_comment = @building.building_comments.find(params[:id])
+    @comments = @building.building_comments.order(created_at: :desc)
+    @building_comment.destroy
+    # redirect_to toilet_path(@toilet)
+    render :index
+
+  end
+
+  # def show
+  #   @toilet = Toilet.find(params[:id])
+  #   @user = User.find(params[:id])
+  #   # @toilet_comments = ToiletComments.find(params[:id])
+  # end
+
+  def building_comment_params
+    params.require(:building_comment).permit(:comment, :image)
+  end
 end
