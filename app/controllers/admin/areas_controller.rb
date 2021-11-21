76f317709f6d3f9b76bf7156d42790e57dc725ab
rikuzyoukurabu <rikuzyoukurabu@gmail.com>
2021-11-21 10:10:44 +0000

    [Fix]レイアウト

--- app/controllers/admin/areas_controller.rb
+++ app/controllers/admin/areas_controller.rb
@@ -1,9 +1,30 @@
 class Admin::AreasController < ApplicationController
+  before_action :authenticate_admin!
   def index
     @areas = Area.all
   end
 
   def show
     @area = Area.find(params[:id])
+  end
+
+
+  def new
+    @area = Area.new
+  end
+
+  def create
+    @area = Area.new(area_params)
+    if @area.save
+      flash[:notice] = '投稿が完了しました！'
+      redirect_to admin_areas_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def area_params
+    params.require(:area).permit(:area_name,:area_id)
   end
 end
--- app/controllers/user/areas_controller.rb
+++ app/controllers/user/areas_controller.rb
@@ -1,14 +1,10 @@
 class User::AreasController < ApplicationController
   def index
     @areas = Area.all
-    # @area = Area.find(params[:id])
   end
 
   def show
     @area = Area.find(params[:id])
-    
-    # @toilet = Toilet.find(params[:id])
-    # @building = Toilet.find(params[:id])
-    
   end
+
 end
